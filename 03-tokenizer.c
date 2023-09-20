8af8efed0a3840dad7010157a5ae90be03c9ceb9
Josiah Ufono <josiahufono@gmail.com>
2023-09-20 14:15:40 -0700

    tokeniser file

---
+++ 03-tokenizer.c
@@ -0,0 +1,49 @@
+#include "shell.h"
+
+/**
+ * tokenizer - Tokenize a string into an array of tokens.
+ * @line: The input string to tokenize.
+ * Return: An array of token strings, or NULL on failure.
+ */
+char **tokenizer(char *line)
+{
+	int m = 0, n = 0;
+	char *token, *tmp, **array = NULL;
+
+	if (!line)
+		return (NULL);
+	tmp = _strdup(line);
+	token = strtok(tmp, DELIM);
+	if (!token)
+	{
+		free(line);
+		free(tmp);
+		tmp = line = NULL;
+		return (NULL);
+	}
+	while (token)
+	{
+		n++;
+		token = strtok(NULL, DELIM);
+	}
+	free(tmp);
+	tmp = NULL;
+	array = malloc(sizeof(char *) * (n + 1));
+	if (!array)
+	{
+		free(line);
+		free(tmp);
+		tmp = line = NULL;
+		return (NULL);
+	}
+	token = strtok(line, DELIM);
+	while (token)
+	{
+		array[m++] = _strdup(token);
+		token = strtok(NULL, DELIM);
+	}
+	array[m] = NULL;
+	free(line);
+	line = NULL;
+	return (array);
+}
